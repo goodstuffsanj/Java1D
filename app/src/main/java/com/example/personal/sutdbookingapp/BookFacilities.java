a8ba625f689e75a79abb65090d5dad44d57c0d55
James Andrew Pohadi <jamesandrewpohadi@gmail.com>
2018-11-09 00:31:23 +0800

    add activities: BookFacilities, BookProf, HomePage

---
+++ app/src/main/java/com/example/personal/sutdbookingapp/BookFacilities.java
@@ -0,0 +1,13 @@
+package com.example.personal.sutdbookingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookFacilities extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_facilities);
+    }
+}
---
+++ app/src/main/java/com/example/personal/sutdbookingapp/BookProf.java
@@ -0,0 +1,13 @@
+package com.example.personal.sutdbookingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BookProf extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_prof);
+    }
+}
