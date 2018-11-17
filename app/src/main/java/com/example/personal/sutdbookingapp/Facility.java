4a5444ab0ec5ac059604206a6791e8fb64bdf6cf
James Andrew Pohadi <jamesandrewpohadi@gmail.com>
2018-11-17 21:44:22 +0800

    testing CustomisableToast

--- app/src/main/java/com/example/personal/sutdbookingapp/Facility.java
+++ app/src/main/java/com/example/personal/sutdbookingapp/Facility.java
@@ -3,12 +3,17 @@ package com.example.personal.sutdbookingapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
 public class Facility extends AppCompatActivity {
     public String facilityId;
     public final static String FACIL_ID = "FACIL_ID";
     public final static String IMAGE = "IMAGE";
     private final static String DESCRIPTION = "DESCRIPTION";
+    Button buttonBookFacility;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,13 @@ public class Facility extends AppCompatActivity {
         String name = intent.getStringExtra(FACIL_ID);
         String image = intent.getStringExtra(IMAGE);
         String desc = intent.getStringExtra(DESCRIPTION);
+        buttonBookFacility = (Button) findViewById(R.id.buttonBookFacility);
+        buttonBookFacility.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StyleableToast.makeText(Facility.this,"hello world",R.style.mytoast).show();
+            }
+        });
         setTitle(name);
     }
 }
