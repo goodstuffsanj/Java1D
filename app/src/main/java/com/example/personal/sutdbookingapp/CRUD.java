b3cca86d1cbbc3980e7f6b7e9de98f66e98c4657
dacong666 <jiaconger@gmail.com>
2018-11-17 10:50:06 +0800

    database updated

---
+++ app/src/main/java/com/example/personal/sutdbookingapp/CRUD.java
@@ -0,0 +1,38 @@
+package com.example.personal.sutdbookingapp;
+
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+
+public class CRUD {
+    private String student_id;
+    private String password;
+    DynamoDBMapper dynamoDBMapper;
+
+    public void createNews() {
+        student_id = "1009";
+        password = "1234";
+        final DBstudent_table.AWS newsItem = new DBstudent_table.AWS();
+
+        newsItem.setStudent_id(student_id);
+        newsItem.setPassword(password);
+        System.out.println("student_id and password are set");
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dynamoDBMapper.save(newsItem);
+                System.out.println("new item is adding");
+                // Item saved
+            }
+        }).start();
+    }
+}
+
+class CRUDtest {
+    static void main(String args[]) {
+        DBstudent_table.AWS aws = new DBstudent_table.AWS();
+        CRUD crud = new CRUD();
+        aws.getStudent_id();
+        aws.getPassword();
+        crud.createNews();
+    }
+}
--- app/src/main/java/com/example/personal/sutdbookingapp/Database.java
+++
@@ -1,7 +0,0 @@
-package com.example.personal.sutdbookingapp;
-
-public class Database {
-    public static class AWS {
-
-    }
-}
