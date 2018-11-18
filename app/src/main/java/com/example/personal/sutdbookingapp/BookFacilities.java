cc03a24aae788c7a43401e24e37cb97eaf832912
Rachel <rachelchuajy@yahoo.com.sg>
2018-11-18 14:59:47 +0800

    update prof page

--- app/src/main/java/com/example/personal/sutdbookingapp/BookFacilities.java
+++ app/src/main/java/com/example/personal/sutdbookingapp/BookFacilities.java
@@ -3,15 +3,12 @@ package com.example.personal.sutdbookingapp;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SearchView;
-import android.util.Log;
 import android.view.Menu;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
