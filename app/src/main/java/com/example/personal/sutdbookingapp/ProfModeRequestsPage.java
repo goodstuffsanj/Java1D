bbe3377776243421ef268a827f0c8e3ac3782c72
Sanjay Shankar <goodstuffsanj@yahoo.com.sg>
2018-11-21 16:51:44 +0800

    added prof mode requests activity

---
+++ app/src/main/java/com/example/personal/sutdbookingapp/ProfModeRequestsPage.java
@@ -0,0 +1,13 @@
+package com.example.personal.sutdbookingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfModeRequestsPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prof_mode_requests_page);
+    }
+}
