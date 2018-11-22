ebe4a40dfc3fcb7420512e0ed287896d54f88ec8
James Andrew Pohadi <jamesandrewpohadi@gmail.com>
2018-11-22 15:46:57 +0800

    resolve conflict

--- app/src/main/java/com/example/personal/sutdbookingapp/Database.java
+++ app/src/main/java/com/example/personal/sutdbookingapp/Database.java
@@ -1,7 +1,36 @@
 package com.example.personal.sutdbookingapp;
 
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+
 public class Database {
-    public static class AWS {
+    static DynamoDBMapper dynamoDBMapper;
+
+    public static class Student {
+        public void createNews(String name, String id) {
+            final NewsDO newsItem = new NewsDO();
+
+            newsItem.setUserId(id);
+
+            newsItem.setArticleId("Article1");
+            newsItem.setContent("This is the article content");
+            newsItem.setAuthor("James");
+
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dynamoDBMapper.save(newsItem);
+                    // Item saved
+                }
+            }).start();
+        }
+    }
+
+    public static class Facility {
 
     }
+
+    public static class BookingInstance {
+
+    }
+
 }
